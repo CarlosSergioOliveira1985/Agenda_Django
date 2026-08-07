f212e52dd7f18208c423a23895ce30b8e1c96f67
Carlos Sergio Oliveira <carlossergio_20@hotmail.com>
2026-08-07 14:29:55 -0300

    Criando Owner para os contatos

--- contact/models.py
+++ contact/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
+    class Meta:
+         verbose_name = 'Category'
+         verbose_name_plural = 'Categories'
+         
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -18,7 +23,17 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%d/')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    category = models.ForeignKey(
+                                Category,
+                                on_delete=models.SET_NULL, 
+                                blank=True, 
+                                null=True)
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, 
+        null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
