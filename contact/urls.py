a4aab8391cf2fee179e6fff00ae13dbf79f03419
Carlos Sergio Oliveira <carlossergio_20@hotmail.com>
2026-08-11 12:53:04 -0300

    inserindo search e links

--- contact/urls.py
+++ contact/urls.py
@@ -5,6 +5,6 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
-    path('', views.index, name='index'),
+    path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
+    path('', views.index, name='index'),  # type:ignore
 ]
