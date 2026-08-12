c423dbea4a017a1c51318cb9e9fc1abafc309b35
Carlos Sergio Oliveira <carlossergio_20@hotmail.com>
2026-08-12 11:22:44 -0300

    criando pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,22 +1,30 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from contact.models import Contact
+from django.core.paginator import Paginator
 from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+
+from contact.models import Contact
 
 
 def index(request):
     contacts = Contact.objects \
-    .filter(show=True) \
-    .order_by('-id')[0:20]
+        .filter(show=True)\
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
+
     return render(
         request,
         'contact/index.html',
         context
     )
+
 
 def search(request):
     search_value = request.GET.get('q', '').strip()
@@ -24,32 +32,40 @@ def search(request):
     if search_value == '':
         return redirect('contact:index')
 
-    
     contacts = Contact.objects \
-    .filter(show=True) \
-    .filter(
-        Q(first_name__icontains=search_value) |
-        Q(last_name__icontains=search_value) |
-        Q(phone__icontains=search_value) |
-        Q(email__icontains=search_value)
-    )\
-    .order_by('-id')
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
-        'site_title': 'Search - '
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
+
     return render(
         request,
         'contact/index.html',
         context
     )
 
+
 def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
-    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
-
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
     site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
         'contact': single_contact,
         'site_title': site_title
